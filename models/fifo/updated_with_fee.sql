619bcd24c10fe54e893d35b730f68436c4762a72
pete <peteryuhsin@gmail.com>
2023-04-03 22:32:22 +0800

    with fee cost

--- models/fifo/updated_with_fee.sql
+++ models/fifo/updated_with_fee.sql
@@ -5,19 +5,19 @@ SELECT
     bought_price,
     bought_qty,
     liquidiate_cost,
-    ROUND((sold_qty / bought_qty) * buy_fee, 5) AS buy_fee,
+    ROUND((sold_qty / bought_qty) * buy_fee, 8) AS buy_fee,
     sell_order_id,
     sold_price,
     sold_qty,
-    ROUND((sold_qty / original_sold_qty) * sell_fee, 5) AS sell_fee,
+    ROUND((sold_qty / orig_qty) * sell_fee, 8) AS sell_fee,
     ROUND(
         proceeds,
-        5
+        8
     ) proceeds,
     date_sold,
     ROUND(
         order_pnL,
-        5
+        8
     ) order_pnL
 FROM
-    {{ ref('update') }}
+    {{ ref('union_original_sold_qty') }}
